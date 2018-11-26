52240591b7096064ebca19e524365e8964bfcfc3
n.ozgurozturk <n.ozgurozturk@hotmail.com>
2018-11-26 15:38:20 +0300

    Remove Footer and Nav items

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,29 +19,20 @@ class Footer extends Component {
                 </a>
                 , made by{" "}
                 <a
-                  href="http://www.nooproject.com/"
+                  href="http://www.nozgurozturk.com/"
                   target="blank"
-                  data-uk-tooltip="Ozgur Ozturk"
+                  data-uk-tooltip="N.Ozgur Ozturk"
                 >
                   me
                 </a>
               </p>
-              <p>
-                
-              </p>
             </div>
             <div className="uk-float-right">
               <a
-                href="https://github.com/nozgurozturk/"
+                href="https://github.com/nozgurozturk/moorse"
                 target="blank"
                 className="uk-icon-link"
                 data-uk-icon="icon: github; ratio:1.5"
-              />
-              <a
-                href="mailto:n.ozgurozturk@gmail.com"
-                target="blank"
-                className="uk-icon-link uk-margin-medium-left"
-                data-uk-icon="icon: mail; ratio:1.5"
               />
             </div>
           </div>
